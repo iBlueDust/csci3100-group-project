879763c67a6b019ed08239d9fd4908b2fb3536ec
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2025-02-20 16:50:23 +0800

    feat<api>: add /auth/signup

---
+++ src/pages/api/auth/signup.ts
@@ -0,0 +1,66 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import Joi from 'joi'
+import { MongoServerError } from "mongodb"
+
+import dbConnect from "@/data/db/mongo"
+import User from "@/data/db/mongo/models/user"
+import { sessionStore } from "@/data/session"
+import { UserRole } from "@/data/types/auth"
+
+type Data = {
+	token: string
+	refreshToken: string
+}
+
+type Error = {
+	code: string,
+	message?: string
+}
+
+async function POST(
+	req: NextApiRequest,
+	res: NextApiResponse<Data | Error>,
+) {
+	const schema = Joi.object({
+		username: Joi.string().pattern(/^[a-zA-Z0-9.-]{1,64}$/).required(),
+		password: Joi.string().pattern(/^[!-~]{8,128}$/).required(), // all ascii printables
+	})
+
+	const { value: body, error } = schema.validate(req.body)
+
+	if (error) {
+		res.status(400).json({ code: 'INVALID_REQUEST', message: error.message })
+	}
+
+	await dbConnect()
+
+	let user: Awaited<ReturnType<typeof User.createWithPassword>>
+	try {
+		user = await User.createWithPassword(body.username, body.password, [UserRole.USER])
+	} catch (error) {
+		if (error instanceof MongoServerError && (error as MongoServerError).code === 11000) { // Duplicate key error code
+			return res.status(409).json({ code: 'USERNAME_TAKEN', message: 'Username is already taken' })
+		}
+		return res.status(500).json({ code: 'INTERNAL_SERVER_ERROR', message: 'An unexpected error occurred' })
+	}
+
+	const session = await sessionStore.createSession(body.username, user.roles)
+
+	res.status(200).json({
+		token: session.token,
+		refreshToken: session.refreshToken,
+	})
+}
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data | Error>,
+) {
+	switch (req.method) {
+		case "POST":
+			return await POST(req, res)
+		default:
+			res.status(405).end() // Method Not Allowed
+	}
+}
