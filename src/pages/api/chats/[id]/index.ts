b7bcbe7f6ee2f842c81fbfa946efef96776f4e5d
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2025-03-27 17:07:37 +0800

    fix<chats>: change `POST /chats/:id` to `POST /chats`
    recipient id is sent as json body
    - recipient: string id

--- src/pages/api/chats/[id]/index.ts
+++ src/pages/api/chats/[id]/index.ts
@@ -3,7 +3,6 @@ import Joi from 'joi'
 
 import dbConnect from '@/data/db/mongo'
 import { Error } from '@/data/types/common'
-import Chat from '@/data/db/mongo/models/chat'
 import { sessionStore } from '@/data/session'
 import { AuthData, protectedRoute } from '@/utils/api/auth'
 import mongoose from 'mongoose'
@@ -45,31 +44,6 @@ async function GET(
 	res.status(200).json(chat)
 }
 
-async function POST(
-	req: NextApiRequest,
-	res: NextApiResponse<Data | Error>,
-	auth: AuthData,
-) {
-	const { value: recipientId, error } = Joi.string().required().validate(req.query.id)
-
-	if (error) {
-		res.status(400).json({ code: 'INVALID_REQUEST', message: error.message })
-		return
-	}
-
-	let recipient: mongoose.Types.ObjectId
-	try {
-		recipient = new mongoose.Types.ObjectId(recipientId)
-	} catch {
-		return res.status(400).json({ code: 'INVALID_REQUEST', message: 'Invalid recipient ID' })
-	}
-
-	await dbConnect()
-	const chat = await Chat.create({ participants: [auth.data.userId, recipient] })
-
-	res.status(200).json({ id: chat.id })
-}
-
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data | Error>,
@@ -77,8 +51,6 @@ export default async function handler(
 	switch (req.method) {
 		case 'GET':
 			return await protectedRoute(GET, sessionStore)(req, res)
-		case 'POST':
-			return await protectedRoute(POST, sessionStore)(req, res)
 		default:
 			res.status(405).end() // Method Not Allowed
 	}
--- src/pages/api/chats/index.ts
+++ src/pages/api/chats/index.ts
@@ -1,19 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Joi from 'joi'
+import mongoose from 'mongoose'
 
 import dbConnect from '@/data/db/mongo'
+import Chat from '@/data/db/mongo/models/chat'
 import { PaginatedResult, Error } from '@/data/types/common'
 import { getRecentChats } from '@/data/db/mongo/queries/chats/getRecentChats'
 import { sessionStore } from '@/data/session'
 import { ChatWithPopulatedFields } from '@/data/types/chats'
 import { AuthData, protectedRoute } from '@/utils/api/auth'
 
-type Data = PaginatedResult<ChatWithPopulatedFields>
+type GetData = PaginatedResult<ChatWithPopulatedFields>
+type PostData = { id: string }
 
 async function GET(
   req: NextApiRequest,
-  res: NextApiResponse<Data | Error>,
+  res: NextApiResponse<GetData | Error>,
   auth: AuthData,
 ) {
   const schema = Joi.object({
@@ -35,13 +38,43 @@ async function GET(
   res.status(200).json(payload)
 }
 
+async function POST(
+  req: NextApiRequest,
+  res: NextApiResponse<PostData | Error>,
+  auth: AuthData,
+) {
+  const schema = Joi.object({
+    recipient: Joi.string().required()
+  })
+  const { value: recipientId, error } = schema.validate(req.body)
+
+  if (error) {
+    res.status(400).json({ code: 'INVALID_REQUEST', message: error.message })
+    return
+  }
+
+  let recipient: mongoose.Types.ObjectId
+  try {
+    recipient = new mongoose.Types.ObjectId(recipientId)
+  } catch {
+    return res.status(400).json({ code: 'INVALID_REQUEST', message: 'Invalid recipient ID' })
+  }
+
+  await dbConnect()
+  const chat = await Chat.create({ participants: [auth.data.userId, recipient] })
+
+  res.status(200).json({ id: chat.id })
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data | Error>,
+  res: NextApiResponse<GetData | PostData | Error>,
 ) {
   switch (req.method) {
     case 'GET':
       return await protectedRoute(GET, sessionStore)(req, res)
+    case 'POST':
+      return await protectedRoute(POST, sessionStore)(req, res)
     default:
       res.status(405).end() // Method Not Allowed
   }
