b2ac1282751227fcdd765db3f560794bb519a28d
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2025-04-24 03:01:15 +0800

    fix: declare next/image minio origin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import env, { isDev } from "@/env"
 import type { NextConfig } from "next"
 
+const matches = /^https?:\/\/([a-zA-Z0-9\._-]*):(\d+)$/.exec(env.MINIO_PUBLIC_ENDPOINT)
+
+const MINIO_PUBLIC_HOST = matches ? matches[1] : env.MINIO_PUBLIC_ENDPOINT
+const MINIO_PUBLIC_PORT = matches ? matches[2] : '80'
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
@@ -9,14 +14,14 @@ const nextConfig: NextConfig = {
     remotePatterns: isDev ? [
       {
         protocol: 'http',
-        hostname: 'localhost',
-        port: '9000',
+        hostname: MINIO_PUBLIC_HOST,
+        port: MINIO_PUBLIC_PORT,
         pathname: `/${env.MINIO_BUCKET_CHAT_ATTACHMENTS}/**`,
       },
       {
         protocol: 'http',
-        hostname: 'localhost',
-        port: '9000',
+        hostname: MINIO_PUBLIC_HOST,
+        port: MINIO_PUBLIC_PORT,
         pathname: `/${env.MINIO_BUCKET_MARKET_LISTING_ATTACHMENTS}/**`,
       },
     ] : [],
